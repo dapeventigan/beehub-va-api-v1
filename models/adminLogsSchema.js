3e000346d7653c49be65f532fa31adeaf2dcbe0e
dapeventigan <dabeventigan@gmail.com>
2024-03-11 16:13:34 +0800

    Initialize admin logs schema

---
+++ models/adminLogsSchema.js
@@ -0,0 +1,21 @@
+// const mongoose = require("mongoose");
+
+// const AdminLogsSchema = new mongoose.Schema({
+//     vaID: String,
+//     certificateTitle: String,
+//     certificateStart: Date,
+//     certificateEnd: Date,
+//     certificate: String,
+// });
+
+// const AdminLogsModel = mongoose.model("AdminLogs", AdminLogsSchema);
+// module.exports = AdminLogsModel;
+
+// Source
+// Created date
+// Date Resume added
+// Created by
+// Last updated
+// Status - New Candidate/Available/Pending Assessments/Pending Interview/Shortlisted/Job Offer/Onboarding/Hired/Under Probation/Probation Passed//Termed
+// Socials (LinkedIn, Facebook, Instagram, Twitter)
+// Skills
